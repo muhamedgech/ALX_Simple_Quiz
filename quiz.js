e245624ccfc613b5b27622436f66fcba6847f25a
Getasew Abeba <muhamedesmil@gmail.com>
2025-01-18 00:03:20 -0800

    second commit

--- quiz.js
+++ quiz.js
@@ -1,17 +1,14 @@
 function checkAnswer() {
     let correctAnswer=4;
-    let userAnswer = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-    if (userAnswer) {
-        // Convert userAnswer's value to a number for comparison
-        if (correctAnswer === Number(userAnswer.value)) {
+   
+        if (userAnswer === correctAnswer){
             document.getElementById("feedback").textContent = "Correct! Well done.";
-        } else {
+        }
+         else {
             document.getElementById("feedback").textContent = "That's incorrect. Try again!";
         }
-    } else {
-        // No option selected
-        document.getElementById("feedback").textContent = "Please select an answer!";
-    }
+   
     // Function body
 }
